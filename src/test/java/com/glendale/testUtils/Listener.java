01513c32e3ef58ebb9852451c64c4c6fbadef51f
pawan <pawan@ARISTA113-PC.Office.Arista>
2024-08-08 14:18:28 +0530

    Added code to the listener class

--- src/test/java/com/glendale/testUtils/Listener.java
+++ src/test/java/com/glendale/testUtils/Listener.java
@@ -1,6 +1,8 @@
 package com.glendale.testUtils;
 
 import java.io.IOException;
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestContext;
@@ -18,8 +20,30 @@ public class Listener extends TestUtils implements ITestListener {
 	ExtentReports extent = TestUtils.getReporterObject();
 
 	public void onTestStart(ITestResult result) {
-		test = extent.createTest(result.getMethod().getMethodName());// Method Name
+		//test = extent.createTest(result.getMethod().getMethodName());// Method Name
+		 String className = result.getTestClass().getName();
+	        String methodName = result.getMethod().getMethodName();
+	        test = extent.createTest(className + " : " + methodName); // Method Name with Class Name
+		test.assignCategory("All");
+		try {
+			List<String> tags = getTicketID(result.getMethod().getMethodName());
+			if (!tags.isEmpty()) {
+				for (String tag : tags) {
+					if (tag.contains("AT-"))
+						test.assignCategory(tag);
+					else
+						test.log(Status.INFO, tag);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
+	}
+
+	List<String> getTicketID(String methodName) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	public void onTestSuccess(ITestResult result) {
@@ -58,6 +82,12 @@ public class Listener extends TestUtils implements ITestListener {
 
 	public void onFinish(ITestContext context) {
 		extent.flush();
+		try {
+			sendReportToEmail();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
